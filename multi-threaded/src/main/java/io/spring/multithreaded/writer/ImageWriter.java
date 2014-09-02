3388e6f87f0d197295bd47f980d246b1dacc8d0e
Chris Schaefer <cschaefer@gopivotal.com>
2014-09-02 18:27:58 -0400

    add multithreaded example

---
+++ multi-threaded/src/main/java/io/spring/multithreaded/writer/ImageWriter.java
@@ -0,0 +1,31 @@
+package io.spring.multithreaded.writer;
+
+import io.spring.multithreaded.domain.ProcessedImage;
+import org.springframework.batch.item.ItemWriter;
+
+import javax.imageio.ImageIO;
+import java.io.File;
+import java.util.List;
+
+/**
+ * <p>
+ * {@link org.springframework.batch.item.ItemWriter} implementation that writes
+ * the {@link io.spring.multithreaded.domain.ProcessedImage}'s to the provided path
+ * using the specified image format.
+ * </p>
+ */
+public class ImageWriter implements ItemWriter<ProcessedImage> {
+	private final String path;
+	private final String formatName;
+
+	public ImageWriter(final String path, final String formatName) {
+		this.path = path;
+		this.formatName = formatName;
+	}
+
+	public void write(final List<? extends ProcessedImage> processedImages) throws Exception {
+		for (ProcessedImage processedImage : processedImages) {
+			ImageIO.write(processedImage.getImage(), formatName, new File(path + processedImage.getFileName()));
+		}
+	}
+}
